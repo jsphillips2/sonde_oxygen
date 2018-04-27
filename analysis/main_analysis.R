5ed62087ba899411209095590354aa2c04de2b23
Unknown <jsphillips2@wisc.edu>
2018-04-27 11:59:28 -0500

    Added figures 5-8

--- analysis/main_analysis.R
+++ analysis/main_analysis.R
@@ -44,14 +44,15 @@ lapply(1:length(post_pred_split), function(x){
   return(y)}) %>% 
   bind_cols %>% 
   select(-stoch1) %>%
-  mutate(Stoch = ifelse(stoch=="obs","Observation Error","Process Error")) %>%
+  mutate(Stoch = ifelse(stoch=="obs","Observation Error","Process Error"),
+         Stoch = factor(Stoch, levels=c("Process Error","Observation Error"))) %>%
   ggplot(aes(real,sim))+
   facet_wrap(~Stoch, nrow=2)+
   geom_point(size=2, alpha=0.5)+
   geom_abline(intercept=0, slope=1)+
-  scale_y_continuous("Chi-squared Simulated",limits=c(4750,5600), 
+  scale_y_continuous(expression(chi^2~"Simulated"), limits=c(4750,5600), 
                      breaks=c(4900,5200,5500))+
-  scale_x_continuous("Chi-squared Real",limits=c(4750,5600), 
+  scale_x_continuous(expression(chi^2~"Real"),limits=c(4750,5600), 
                      breaks=c(4900,5200,5500))+
   coord_equal()+
   theme_base
@@ -60,7 +61,7 @@ lapply(1:length(post_pred_split), function(x){
 
 
 #==========
-#========== Figure 2: Total Daily Fluxes
+#========== Figure 2: Net Daily Fluxes
 #==========
 
 model_fit %>%
@@ -132,10 +133,11 @@ model_fit %>%
               summarize(day = unique(D_M))) %>%
   mutate(middle = middle/1000,
          lower16 = lower16/1000,
-         upper84 = upper84/1000) %>%
+         upper84 = upper84/1000,
+         name = factor(name, levels=c("beta0","rho"), labels=c("\u03B2","\u03C1"))) %>%
   arrange(year,yday) %>%
   ggplot(aes(yday, middle, color=name))+
-  facet_wrap(~year)+
+  facet_wrap(~year, labeller=label_parsed)+
   geom_ribbon(aes(ymin=lower16, ymax=upper84, fill=name),
                 linetype=0, alpha=0.35)+
   geom_line(size=0.6)+
@@ -148,6 +150,127 @@ model_fit %>%
 
 
 
+#==========
+#========== Figure 5: RESP vs. GPP
+#==========
+
+model_fit %>%
+  filter(name %in% c("GPP","ER")) %>%
+  left_join(sonde_data %>%
+              group_by(year, yday) %>%
+              summarize(day = unique(D_M))) %>%
+  select(-lower16, -upper84) %>%
+  mutate(middle = middle/1000) %>%
+  spread(name, middle) %>%
+  ggplot(aes(GPP, ER, color=factor(year)))+
+  geom_point(size=2)+
+  geom_abline(intercept=0, slope=1)+
+  scale_color_brewer("",type = "div", palette="Spectral")+
+  scale_y_continuous(expression("ER (g "*O[2]~m^{-2}~day^{-1}*")"), limits=c(2,9))+
+  scale_x_continuous(expression("GPP (g "*O[2]~m^{-2}~day^{-1}*")"), limits=c(2,9))+
+  coord_equal()+
+  theme_base
+
+  
 
 
+
+
+#==========
+#========== Figure 6: beta0 vs. rho
+#==========
+
+model_fit %>%
+  filter(name %in% c("beta0","rho")) %>%
+  left_join(sonde_data %>%
+              group_by(year, yday) %>%
+              summarize(day = unique(D_M))) %>%
+  select(-lower16, -upper84) %>%
+  mutate(middle = middle/1000) %>%
+  spread(name, middle) %>%
+  {ggplot(.,aes(beta0, rho, color=factor(year)))+
+      geom_path(size=0.8, alpha=0.8,
+                arrow = arrow(angle = 20, length=unit(0.4,"cm"), type = "closed",ends = "last"))+
+      geom_point(data=. %>% group_by(year) %>% summarize(beta0 = beta0[1], rho = rho[1]),
+                 size = 3)+
+      scale_color_brewer("",type = "div", palette="Spectral")+
+      scale_y_continuous(expression("\u03C1 (g "*O[2]~m^{-2}~day^{-1}*")"))+
+      scale_x_continuous(expression("\u03B2 (g "*O[2]~m^{-2}~day^{-1}*")"))+
+      theme_base}
+
+
+
+
+
+#==========
+#========== Figure 7: GPP vs. predictors
+#==========
+
+model_fit %>%
+  filter(name %in% c("GPP","beta0")) %>%
+  left_join(sonde_data %>%
+              group_by(year, yday) %>%
+              summarize(day = unique(D_M))) %>%
+  select(-lower16, -upper84, -index, -day) %>%
+  bind_rows(sonde_data %>%
+              group_by(year, yday) %>%
+              summarize(day = unique(D_M), par = mean(par), temp = mean(temp)) %>%
+              gather(name, middle, c(par,temp)) %>%
+              select(-day)) %>%
+  mutate(name = factor(name, levels=c("temp","par","beta0","GPP"))) %>%
+  group_by(name) %>%
+  mutate(middle = (middle - mean(middle, na.rm=T))/sd(middle, na.rm=T)) %>%
+  spread(name, middle) %>%
+  gather(var, value, c(temp,par,beta0)) %>%
+  mutate(var = factor(var, levels = c("temp","par","beta0"),
+                      labels=c("Temperature (scaled)",
+                               "PAR (scaled)", "\u03B2 (scaled)"))) %>%
+  ggplot(aes(value, GPP, color=factor(year)))+
+  facet_wrap(~var, nrow=3, strip.position="bottom")+
+  geom_point()+
+  scale_color_brewer("",type = "div", palette="Spectral")+
+  scale_x_continuous("")+
+  scale_y_continuous("GPP (scaled)")+
+  theme_base+
+  theme(axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        strip.text = element_text(size=12))
+
+
+
+
+
+#==========
+#========== Figure 8: ER vs. predictors
+#==========
+
+model_fit %>%
+  filter(name %in% c("ER","rho")) %>%
+  left_join(sonde_data %>%
+              group_by(year, yday) %>%
+              summarize(day = unique(D_M))) %>%
+  select(-lower16, -upper84, -index, -day) %>%
+  bind_rows(sonde_data %>%
+              group_by(year, yday) %>%
+              summarize(day = unique(D_M), par = mean(par), temp = mean(temp)) %>%
+              gather(name, middle, c(par,temp)) %>%
+              select(-day)) %>%
+  mutate(name = factor(name, levels=c("temp","par","rho","ER"))) %>%
+  group_by(name) %>%
+  mutate(middle = (middle - mean(middle, na.rm=T))/sd(middle, na.rm=T)) %>%
+  spread(name, middle) %>%
+  gather(var, value, c(temp,par,rho)) %>%
+  mutate(var = factor(var, levels = c("temp","par","rho"),
+                      labels=c("Temperature (scaled)",
+                               "PAR (scaled)", "\u03C1 (scaled)"))) %>%
+  ggplot(aes(value, ER, color=factor(year)))+
+  facet_wrap(~var, nrow=3, strip.position="bottom")+
+  geom_point()+
+  scale_color_brewer("",type = "div", palette="Spectral")+
+  scale_x_continuous("")+
+  scale_y_continuous("ER (scaled)")+
+  theme_base+
+  theme(axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        strip.text = element_text(size=12))
 
