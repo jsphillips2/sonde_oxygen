4579fb9f3fd07f753d10a45a223831c0308af27e
Unknown <jsphillips2@wisc.edu>
2018-05-21 16:38:14 -0500

    Updated exported file locations

--- simulations/fit_16/main_analysis_16.R
+++ simulations/fit_16/main_analysis_16.R
@@ -7,9 +7,9 @@ library(tidyverse)
 
 # import data and model fit
 sonde_data = read_csv("data/sonde_prep.csv")
-params_full = read_csv("main_analysis/model_output/fixed_pars_full.csv")
-post_pred = read_csv("main_analysis/model_output/post_pred_full.csv")
-model_fit = read_csv("main_analysis/model_output/summary_clean.csv")
+params_full = read_csv("simulations/fit_16/model_output_16/fixed_pars_full.csv")
+post_pred = read_csv("simulations/fit_16/model_output_16/post_pred_full.csv")
+model_fit = read_csv("simulations/fit_16/model_output_16/summary_clean.csv")
 
 # base theme
 theme_base = theme_bw()+
