469b0879c09a7228035cef3ac42784469747023d
Unknown <jsphillips2@wisc.edu>
2018-09-10 18:19:06 -0500

    Updated priors to make more vague

--- analyses/test_analysis/model_fit/fit_model.R
+++ analyses/test_analysis/model_fit/fit_model.R
@@ -27,24 +27,26 @@ data = read_rdump("analyses/test_analysis/model_fit/input/sonde_list.R")
 # priors
 priors = list(k0_prior = c(2, 1),
               k1_prior = c(0.2, 0.1),
-              gamma_1_prior = c(1.1, 0.5),
-              gamma_2_prior = c(1.1, 0.5),
-              sig_beta0_prior = c(0, 0.5),
-              sig_alpha_prior = c(0, 0.05),
-              sig_rho_prior = c(0, 0.5),
+              gamma_1_prior = c(1.1, 1),
+              gamma_2_prior = c(1.1, 1),
+              sig_beta0_prior = c(0, 1),
+              sig_alpha_prior = c(0, 1),
+              sig_rho_prior = c(0, 1),
               sig_proc_prior = c(100, 100),
-              log_beta0_prior = c(5.5, 5.5),
-              log_alpha_prior = c(1, 1),
-              log_rho_prior = c(5.5, 5.5))
+              log_beta0_prior = c(5, 7),
+              log_alpha_prior = c(2, 4),
+              log_rho_prior = c(5, 7))
 
 # examine priors (change name and bounds as appropriate)
-priors$k0_prior %>%
-  {rtruncnorm(n = 50000, a = 0, b = Inf, mean = .[1], sd = .[2])} %>%
-  hist
+# priors$k0_prior %>%
+#   {rtruncnorm(n = 50000, a = 0, b = Inf, mean = .[1], sd = .[2])} %>%
+#   hist
 
 # export priors
-# as_data_frame(append(list(par = c("mean","sd")), priors)) %>%
-#   write_csv("analyses/test_analysis/model_fit/input/priors.csv")
+as_data_frame(append(list(par = c("mean","sd")), priors)) %>%
+  gather(name, value, 2:12) %>%
+  spread(par, value) %>%
+  write_csv("analyses/test_analysis/model_fit/input/priors.csv")
 
 # add priors to data
 data_full = data %>% append(priors)
@@ -56,15 +58,15 @@ init_fn = function(){
        gamma_1 = runif(n = 1, min = 1, max = 2),
        gamma_2 = runif(n = 1, min = 1, max = 2),
        sig_beta = runif(n = 1, min = 0, max = 0.2),
-       sig_alpha = runif(n = 1, min = 0, max = 0.02),
+       sig_alpha = runif(n = 1, min = 0, max = 0.2),
        sig_rho = runif(n = 1, min = 0, max = 0.2),
        sig_proc = runif(n = 1, min = 50, max = 150),
-       log_beta0_init = runif(n = data$Y + 1, min = log(0.1) + 5.5, 
-                           max = log(1.9) + 5.5),
-       log_alpha_init = runif(n = data$Y + 1, min = log(0.1) + 1, 
-                           max = log(1.9) + 1),
-       log_rho_init = runif(n = data$Y + 1, min = log(0.1) + 5.5, 
-                         max = log(1.9) + 5.5)
+       log_beta0_init = runif(n = data$Y + 1, min = log(0.1) + 5, 
+                           max = log(1.9) + 5),
+       log_alpha_init = runif(n = data$Y + 1, min = log(0.1) + 2, 
+                           max = log(1.9) + 2),
+       log_rho_init = runif(n = data$Y + 1, min = log(0.1) + 5, 
+                         max = log(1.9) + 5)
   )
 }
 
@@ -128,7 +130,7 @@ fixed_pars %>%
   theme_bw()
 
 # pairs plot for parameters
-ggpairs(fixed_pars %>% select(-chain, -step))
+# ggpairs(fixed_pars %>% select(-chain, -step))
 
 
 
@@ -189,7 +191,7 @@ fit_clean = fit_summary %>%
   select(name, index, day, middle, lower16, upper84) %>%
   filter(!(name %in% c("log_beta0","log_rho","lp__")))
 
-# Export
+# export
 output_path = "analyses/test_analysis/model_fit/output"
 # write_csv(fixed_pars, paste0(output_path,"/fixed_pars_full.csv"))
 # write_csv(post_pred, paste0(output_path,"/post_pred_full.csv"))
