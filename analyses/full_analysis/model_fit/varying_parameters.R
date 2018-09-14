1e26e2c64da52f9774bbe524d300a08009b7c094
Unknown <jsphillips2@wisc.edu>
2018-09-14 15:31:57 -0500

    Updated for full analysis; added ecosystem metabolism estimates

--- analyses/full_analysis/model_fit/varying_parameters.R
+++ analyses/full_analysis/model_fit/varying_parameters.R
@@ -8,7 +8,7 @@ library(GGally)
 library(truncnorm)
 
 # import data and model fit
-input_dir = "analyses/test_analysis/model_fit/"
+input_dir = "analyses/full_analysis/model_fit/"
 sonde_data = read_csv(paste0(input_dir,"input/sonde_prep.csv"))
 priors = read_csv(paste0(input_dir,"input/priors.csv"))
 model_fit = read_csv(paste0(input_dir,
@@ -30,13 +30,52 @@ theme_base = theme_bw()+
 
 
 
+#==========
+#========== Daily Metabolism
+#==========
+
+# plot
+model_fit %>%
+  filter(name %in% c("GPP","ER","NEP")) %>%
+  left_join(sonde_data %>%
+              group_by(year, yday) %>%
+              summarize(day = unique(D_M))) %>%
+  full_join(sonde_data %>% expand(year,yday,name=c("GPP","ER","NEP"))) %>%
+  mutate(middle = ifelse(name=="ER",-middle,middle)/1000,
+         lower16 = ifelse(name=="ER",-lower16,lower16)/1000,
+         upper84 = ifelse(name=="ER",-upper84,upper84)/1000,
+         name = factor(name, levels=c("GPP","NEP","ER"))) %>%
+         {ggplot(., aes(yday, middle))+
+             facet_wrap(~year)+
+             geom_hline(yintercept = 0, alpha=0.5, size=0.2)+
+             geom_ribbon(data = . %>% filter(is.na(middle) == F), aes(ymin=lower16, ymax=upper84, group = name),
+                         linetype=0, alpha=0.35)+
+             geom_line(aes(linetype=name), size=0.6)+
+             scale_linetype_manual("",
+                                   values=c(1,3,2),
+                                   guide = guide_legend(direction = "horizontal",
+                                     label.position = "top"))+
+             scale_y_continuous(expression(Metabolism~
+                                             "("*g~O[2]~m^{-2}~day^{-1}*")"))+
+             # scale_x_continuous("Day of Year", 
+             #                    limits=c(151,201), breaks=c(160,176,192))+
+             theme_base+
+             theme(legend.position = "top",
+                   legend.key.width = unit(3, "line"),
+                   legend.key.height = unit(0.8, "line"),
+                   legend.text.align = 0.5)
+         }
+
+
+
+
 
 #==========
-#========== beta and rho
+#========== beta0 and rho
 #==========
 
-# prep data
-beta_rho = model_fit %>%
+# prepare data
+met_p = model_fit %>%
   filter(name %in% c("beta0","rho")) %>%
   left_join(sonde_data %>%
               group_by(year, yday) %>%
@@ -47,24 +86,25 @@ beta_rho = model_fit %>%
   arrange(year,yday)
 
 # plot
-beta_rho %>%
-  ggplot(aes(yday, middle, color=name))+
-  facet_wrap(~year, labeller=label_parsed)+
-  geom_hline(yintercept = 0.3, alpha=0.5, size=0.2)+
-  geom_ribbon(aes(ymin=lower16, ymax=upper84, fill=name),
+met_p %>%
+  ggplot(aes(yday, middle, linetype=name))+
+  facet_wrap(~year)+
+  geom_ribbon(aes(ymin=lower16, ymax=upper84, group=name),
               linetype=0, alpha=0.35)+
   geom_line(size=0.6)+
-  scale_color_manual("",values=c("dodgerblue","firebrick"), 
-                     labels = c(expression(beta^0), expression(rho)))+
-  scale_fill_manual("",values=c("dodgerblue","firebrick"), 
-                    labels = c(expression(beta^0), expression(rho)))+
-  scale_y_continuous(expression("Metabolism Parameter (g "*O[2]~m^{-2}~h^{-1}*")"),
-                     breaks=c(0.15,0.3,0.45))+
-  scale_x_continuous("Day of Year", 
-                     limits=c(151,201), breaks=c(160,176,192))+
+  geom_hline(yintercept = 0.3, size = 0.3, alpha = 0.5)+
+  scale_linetype_manual("",values=c(1,2),
+                        guide = guide_legend(direction = "horizontal",
+                          label.position = "top"),
+                        labels = c(expression(beta^0), expression(rho)))+
+  scale_y_continuous(expression("Metabolism Parameter (g "*O[2]~m^{-2}~h^{-1}*")"))+
+  # scale_x_continuous("Day of Year", 
+  #                    limits=c(151,201), breaks=c(160,176,192))+
   theme_base+
-  theme(legend.text.align = 0,
-        legend.position = c(0.2,0.85))
+  theme(legend.position = "top",
+        legend.key.width = unit(3, "line"),
+        legend.key.height = unit(0.8, "line"),
+        legend.text.align = 0.5)
 
 
 
@@ -85,14 +125,13 @@ model_fit %>%
   arrange(year,yday) %>%
   ggplot(aes(yday, middle))+
   facet_wrap(~year)+
+  geom_hline(yintercept = 3, size = 0.3, alpha = 0.5)+
   geom_ribbon(aes(ymin=lower16, ymax=upper84),
               linetype=0, alpha=0.35)+
   geom_line(size=0.6)+
   scale_y_continuous(expression(alpha~"("*mg~O[2]~s~mu*mol-photons^{-1}~h^{-1}*")"))+
-  scale_x_continuous("Day of Year", 
-                     limits=c(151,201), breaks=c(160,176,192))+
+  scale_x_continuous("Day of Year")+
   theme_base
-
 
 
 
